c1ccace1c1710b1649e510516d7449127bf67d88
cvu1998 <congvinhvu@homail.com>
2019-09-17 23:01:46 -0400

    Added Initial Cards Shuffling
    -Added function to shuffle cards initially
    -(To do) Fix opening of GameActivity twice when play button clicked

--- app/src/main/java/com/game/matcher/CardPair.java
+++ app/src/main/java/com/game/matcher/CardPair.java
@@ -10,12 +10,22 @@ public class CardPair {
     private Drawable backImage;
     private Drawable frontImage;
     private boolean openned;
+    private int firstID;
+    private int secondID;
 
-    public CardPair(Context context, Drawable image) {
+    public CardPair(Context context, int ID1, int ID2) {
         Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.tinder);
-        backImage = new BitmapDrawable(context.getResources(), bitmap);;
-        frontImage = image;
+        backImage = new BitmapDrawable(context.getResources(), bitmap);
         openned = false;
+        firstID = ID1;
+        secondID = ID2;
+    }
+
+    public int getFirstID() {
+        return firstID;
+    }
+    public int getSecondID() {
+        return secondID;
     }
 
     public void setOpenned(boolean bool) {
@@ -28,6 +38,10 @@ public class CardPair {
 
     public Drawable getBackImage() {
         return backImage;
+    }
+
+    public void setFrontImage(Drawable frontImage) {
+        this.frontImage = frontImage;
     }
 
     public Drawable getFrontImage() {
--- app/src/main/java/com/game/matcher/GameActivity.java
+++ app/src/main/java/com/game/matcher/GameActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -18,23 +20,59 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        generateCardsImage(10);
+        shuffleCards(10);
+        for (CardPair p : cardsPairs) {
+            Log.d("Pairs", Integer.toString(p.getFirstID()) + " and " + Integer.toString(p.getSecondID()));
+        }
     }
 
-    private void generateCards(int numberOfCards){
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Clear everything
+        //buttons.clear();
+        cardsPairs.clear();
     }
 
     private void shuffleCards(int numberOfCards){
+        ArrayList<Integer> cardsID = new ArrayList<Integer>();
+        for(int i = 0; i < numberOfCards; ++i) {
+            cardsID.add(i);
+        }
+        int nRolls = 0;
+        int ID1 = -1;
+        int ID2 = -1;
+        Random rnd = new Random();
+        while(cardsPairs.size() < (numberOfCards / 2)) {
+            if (nRolls == 0) {
+                int index = rnd.nextInt(cardsID.size());
+                ID1 = cardsID.get(index);
+                cardsID.remove(index);
+                ++nRolls;
+            } else if (nRolls == 1) {
+                int index = rnd.nextInt(cardsID.size());
+                ID2 = cardsID.get(index);
+                cardsID.remove(index);
+                ++nRolls;
+            } else if (ID1 != -1 && ID2 != -1) {
+                cardsPairs.add(new CardPair(this, ID1, ID2));
+                nRolls = 0;
+            }
+        }
+    }
 
+    private void generateCardsImage(int numberOfCards){
+        cardsPairs = new ArrayList<CardPair>();
     }
 
     private void setCards(int numberOfCards) {
-        buttons = new ArrayList<ImageButton>();
-        cardsPairs = new ArrayList<CardPair>();
+        /*buttons = new ArrayList<ImageButton>();
         for(int i = 0; i < numberOfCards; ++i) {
             ImageButton btn = new ImageButton(this);
             btn.setImageDrawable(cardsPairs.get(i).getBackImage());
             buttons.add(btn);
-        }
+        }*/
     }
 }
