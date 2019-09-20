c6f41a8e3d59ccd05ba9e52299a83d47898de915
cvu1998 <congvinhvu@homail.com>
2019-09-20 17:03:09 -0400

    Dynamic Card Creation
    -Cards are no longer generated statically

--- app/src/main/java/com/game/matcher/GameActivity.java
+++ app/src/main/java/com/game/matcher/GameActivity.java
@@ -11,11 +11,15 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -35,22 +39,12 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
 
     private int cardsFlipped = 0;
-    private ArrayList<ImageButton> buttons;
+    private ArrayList<Button> buttons;
     private ArrayList<Integer> cardsFlippedIndex;
     private ArrayList<CardPair> cardsPairs;
     private Drawable drawable;
     private ArrayList<String> ImagesURLs;
     private ArrayList<Integer> mapCardsToButtons;
-    private ImageButton b0;
-    private ImageButton b1;
-    private ImageButton b2;
-    private ImageButton b3;
-    private ImageButton b4;
-    private ImageButton b5;
-    private ImageButton b6;
-    private ImageButton b7;
-    private ImageButton b8;
-    private ImageButton b9;
     private boolean isDone;
     private int matched;
     private TableLayout cardsTable;
@@ -68,9 +62,9 @@ public class GameActivity extends AppCompatActivity {
 
         matched = 0;
 
-        shuffleCards(10);
-        generateCardsFrontImage(10);
-        setCards(10);
+        shuffleCards(20);
+        generateCardsFrontImage(20);
+        setCards(20);
         for (CardPair p : cardsPairs) {
             Log.d("Pairs", Integer.toString(p.getFirstID()) + " and " + Integer.toString(p.getSecondID()));
         }
@@ -133,7 +127,7 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void setCards(int numberOfCards) {
-        buttons = new ArrayList<ImageButton>();
+        buttons = new ArrayList<Button>();
         cardsFlippedIndex = new ArrayList<Integer>();
         mapCardsToButtons = new ArrayList<Integer>() ;
         int index = -1;
@@ -143,7 +137,7 @@ public class GameActivity extends AppCompatActivity {
                 mapCardsToButtons.add(index);
             }
         };
-        setCardsFlipping();
+        setCardsFlipping(numberOfCards);
     }
 
     private int getMatchingID(int index) {
@@ -155,120 +149,63 @@ public class GameActivity extends AppCompatActivity {
         return -1;
     }
 
-    private void setCardsFlipping() {
-        b0 = findViewById(R.id.Button0);
-        b0.setImageResource(R.drawable.logo);
-        b1 = findViewById(R.id.Button1);
-        b1.setImageResource(R.drawable.logo);
-        b2 = findViewById(R.id.Button2);
-        b2.setImageResource(R.drawable.logo);
-        b3 = findViewById(R.id.Button3);
-        b3.setImageResource(R.drawable.logo);
-        b4 = findViewById(R.id.Button4);
-        b4.setImageResource(R.drawable.logo);
-        b5 = findViewById(R.id.Button5);
-        b5.setImageResource(R.drawable.logo);
-        b6 = findViewById(R.id.Button6);
-        b6.setImageResource(R.drawable.logo);
-        b7 = findViewById(R.id.Button7);
-        b7.setImageResource(R.drawable.logo);
-        b8 = findViewById(R.id.Button8);
-        b8.setImageResource(R.drawable.logo);
-        b9 = findViewById(R.id.Button9);
-        b9.setImageResource(R.drawable.logo);
-
-        b0.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) { ;
-                cardsFlippedIndex.add(0);
-                int index = mapCardsToButtons.get(0);
-                updateCardsPairs(0, cardsPairs.get(index), b0, buttons.get(cardsPairs.get(index).getSecondID()));
+    private void setCardsFlipping(int numberOfCards) {
+        int z = 0;
+        for (int i = 0; i < 4; ++i) {
+            TableRow tr = findViewById(R.id.row1);
+            if (i == 1) {
+                tr = findViewById(R.id.row2);
+                Log.d("tablerow", "2");
             }
-        });
-        b1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(1);
-                int index = mapCardsToButtons.get(1);
-                updateCardsPairs(1, cardsPairs.get(index), b1, buttons.get(cardsPairs.get(index).getSecondID()));
+            else if (i == 2) {
+                tr = findViewById(R.id.row3);
+                Log.d("tablerow", "3");
             }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(2);
-                int index = mapCardsToButtons.get(2);
-                updateCardsPairs(2, cardsPairs.get(index), b2, buttons.get(cardsPairs.get(index).getSecondID()));
+            else if (i == 3){
+                tr = findViewById(R.id.row4);
+                Log.d("tablerow", "4");
             }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(3);
-                int index = mapCardsToButtons.get(3);
-                updateCardsPairs(3, cardsPairs.get(index), b3, buttons.get(cardsPairs.get(index).getSecondID()));
+            for (int j = 0; j < 5; ++j) {
+                Button card = new Button(this);
+                int paddingDp = 100 / numberOfCards;
+                TableRow.LayoutParams params = new TableRow.LayoutParams(1800 / numberOfCards, 3900 / numberOfCards, 1);
+                params.setMargins(paddingDp,paddingDp,paddingDp,paddingDp);
+                card.setLayoutParams(params);
+                card.setBackgroundResource(R.drawable.logo);
+                card.setTag(z);
+                card.setVisibility(View.VISIBLE);
+                card.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        int buttonIndex = (Integer)v.getTag();
+                        cardsFlippedIndex.add(buttonIndex);
+                        int index = mapCardsToButtons.get(buttonIndex);
+                        updateCardsPairs(buttonIndex, cardsPairs.get(index), buttons.get(cardsPairs.get(index).getFirstID()), buttons.get(cardsPairs.get(index).getSecondID()));
+                    }
+                });
+                buttons.add(card);
+                tr.addView(card);
+                ++z;
             }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(4);
-                int index = mapCardsToButtons.get(4);
-                updateCardsPairs(4, cardsPairs.get(index), b4, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(5);
-                int index = mapCardsToButtons.get(5);
-                updateCardsPairs(5, cardsPairs.get(index), b5, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(6);
-                int index = mapCardsToButtons.get(6);
-                updateCardsPairs(6, cardsPairs.get(index), b6, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(7);
-                int index = mapCardsToButtons.get(7);
-                updateCardsPairs(7, cardsPairs.get(index), b7, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(8);
-                int index = mapCardsToButtons.get(8);
-                updateCardsPairs(8, cardsPairs.get(index), b8, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                cardsFlippedIndex.add(9);
-                int index = mapCardsToButtons.get(9);
-                updateCardsPairs(9, cardsPairs.get(index), b9, buttons.get(cardsPairs.get(index).getSecondID()));
-            }
-        });
-        buttons.add(b0);
-        buttons.add(b1);
-        buttons.add(b2);
-        buttons.add(b3);
-        buttons.add(b4);
-        buttons.add(b5);
-        buttons.add(b6);
-        buttons.add(b7);
-        buttons.add(b8);
-        buttons.add(b9);
+        }
     }
 
-    private void updateCardsPairs(int iD, CardPair pair, ImageButton firstCard, ImageButton secondCard) {
+    /*private int findCardsIndex(Object o) {
+        for (int i = 0; i < buttons.size(); ++i) {
+            if ((Integer)buttons.get(i).getTag() == i) {
+                return i;
+            }
+        }
+        return -1;
+    }*/
+
+    private void updateCardsPairs(int iD, CardPair pair, Button firstCard, Button secondCard) {
         if (iD == pair.getFirstID()) {
             if (pair.getMode() == 0) {
-                firstCard.setImageDrawable(pair.getFrontImage());
-                firstCard.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                firstCard.setBackground(pair.getFrontImage());
                 firstCard.setEnabled(false);
                 pair.setMode(1);
             } else if (pair.getMode() == 2) {
-                firstCard.setImageDrawable(pair.getFrontImage());
-                firstCard.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                firstCard.setBackground(pair.getFrontImage());
                 firstCard.setEnabled(false);
                 secondCard.setEnabled(false);
                 pair.setMode(3);;
@@ -276,13 +213,11 @@ public class GameActivity extends AppCompatActivity {
         }
         else {
             if (pair.getMode() == 0) {
-                secondCard.setImageDrawable(pair.getFrontImage());
-                secondCard.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                secondCard.setBackground(pair.getFrontImage());
                 secondCard.setEnabled(false);
                 pair.setMode(2);
             } else if (pair.getMode() == 1) {
-                secondCard.setImageDrawable(pair.getFrontImage());
-                secondCard.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                secondCard.setBackground(pair.getFrontImage());
                 firstCard.setEnabled(false);
                 secondCard.setEnabled(false);
                 pair.setMode(3);
@@ -313,11 +248,9 @@ public class GameActivity extends AppCompatActivity {
     private void flipCardsBack() {
         CardPair pair0 =  cardsPairs.get(mapCardsToButtons.get(cardsFlippedIndex.get(0)));
         CardPair pair1 =  cardsPairs.get(mapCardsToButtons.get(cardsFlippedIndex.get(1)));
-        buttons.get(cardsFlippedIndex.get(0)).setImageResource(R.drawable.logo);
-        buttons.get(cardsFlippedIndex.get(0)).setScaleType(ImageView.ScaleType.FIT_XY);
+        buttons.get(cardsFlippedIndex.get(0)).setBackgroundResource(R.drawable.logo);
         buttons.get(cardsFlippedIndex.get(0)).setEnabled(true);
-        buttons.get(cardsFlippedIndex.get(1)).setImageResource(R.drawable.logo);
-        buttons.get(cardsFlippedIndex.get(1)).setScaleType(ImageView.ScaleType.FIT_XY);
+        buttons.get(cardsFlippedIndex.get(1)).setBackgroundResource(R.drawable.logo);
         buttons.get(cardsFlippedIndex.get(1)).setEnabled(true);
         pair0.setMode(0);
         pair1.setMode(0);
